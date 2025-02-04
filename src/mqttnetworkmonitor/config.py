83e4e136b4d62221ce2f3d8e8e38454a3ccbb174
Bryan Wyatt <brwyatt@gmail.com>
2025-02-04 09:43:22 -0800

    Add additional/better config validation

--- src/mqttnetworkmonitor/config.py
+++ src/mqttnetworkmonitor/config.py
@@ -25,13 +25,13 @@ class ConnectionConfig:
         self._password = file_data.get("password", password)
 
         if self._host is None:
-            raise ValueError("Host is required")
+            raise ValueError(f"Host is required, but not provided explicitly or in {file_name}")
         if self._port is None:
-            raise ValueError("Port is required")
+            raise ValueError(f"Port is required, but not provided explicitly or in {file_name}")
         if self._username is None:
-            raise ValueError("Username is required")
+            raise ValueError(f"Username is required, but not provided explicitly or in {file_name}")
         if self._password is None:
-            raise ValueError("Password is required")
+            raise ValueError(f"Password is required, but not provided explicitly or in {file_name}")
 
     def __getitem__(self, key):
         if not key in ["host", "port", "username", "password"]:
@@ -46,6 +46,18 @@ class Config:
                 self._data = yaml.safe_load(file)
         except FileNotFoundError:
             raise ValueError(f"Config file {file_name} not found")
+
+        if not isinstance(self._data.get("site"), dict):
+            raise ValueError("Missing 'site' in config file")
+
+        if not isinstance(self._data.get("site", {}).get("name"), str):
+            raise ValueError("Missing 'site.name' in config file or not a string")
+
+        if not isinstance(self._data.get("site", {}).get("domain"), str):
+            raise ValueError("Missing 'site.domain' in config file or not a string")
+
+        if not isinstance(self._data.get("site", {}).get("interfaces"), int):
+            raise ValueError("Missing 'site.interfaces' in config file or not a number")
 
     def __getitem__(self, key):
         return self._data[key]
