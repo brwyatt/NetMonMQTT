c9a480d0681ac4125790bd0faab20d9fb0cc5675
Bryan Wyatt <brwyatt@gmail.com>
2025-02-08 11:45:09 -0800

    Add "site_name" to config module

--- src/netmonmqtt/config.py
+++ src/netmonmqtt/config.py
@@ -10,6 +10,7 @@ class Config:
         port: Optional[int] = None,
         username: Optional[str] = None,
         password: Optional[str] = None,
+        site_name: Optional[str] = None,
     ):
         file_data = {}
         if file_name:
@@ -23,6 +24,7 @@ class Config:
         self._port = file_data.get("port", port)
         self._username = file_data.get("username", username)
         self._password = file_data.get("password", password)
+        self._site_name = file_data.get("site_name", site_name)
 
         if self._host is None:
             raise ValueError(f"Host is required, but not provided explicitly or in {file_name}")
@@ -32,8 +34,11 @@ class Config:
             raise ValueError(f"Username is required, but not provided explicitly or in {file_name}")
         if self._password is None:
             raise ValueError(f"Password is required, but not provided explicitly or in {file_name}")
+        if self._site_name is None:
+            raise ValueError(f"Site Name is required, but not provided explicitly or in {file_name}")
+
 
     def __getitem__(self, key):
-        if not key in ["host", "port", "username", "password"]:
+        if not key in ["host", "port", "username", "password", "site_name"]:
             raise KeyError(f"{key} is not a valid key")
         return getattr(self, f"_{key}")
