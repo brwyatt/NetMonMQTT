2c6ea60796331dd96e6a64312d8eb234e9daa83f
Bryan Wyatt <brwyatt@gmail.com>
2025-02-08 10:10:18 -0800

    Add ping check test

---
+++ src/netmonmqtt/checks/ping.py
@@ -0,0 +1,21 @@
+from typing import Tuple
+from pythonping import ping
+
+
+def check_ping(
+    target: str,
+    timeout: int = 2,
+    count: int = 1,
+    min_success_ratio: float = 1,
+) -> Tuple[bool, float]:
+    if min_success_ratio > 1:
+        min_success_ratio = 1
+    elif min_success_ratio < 0:
+        min_success_ratio = 0
+
+    try:
+        result = ping(target, timeout=timeout, count=count)
+    except Exception as e:
+        return False, 0
+    
+    return result.stats_success_ratio >= min_success_ratio, result.rtt_avg
